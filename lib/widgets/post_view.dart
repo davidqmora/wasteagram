20f4dc6b89a9b2ec4e4ecd44ab22766764579880
David Mora <davidqmora@gmail.com>
2020-08-09 15:11:03 -0700

    Small mods to post view to mock all data that is supposed to be displayed.

--- lib/widgets/post_view.dart
+++ lib/widgets/post_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -12,16 +13,18 @@ class PostView extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        title(context),
-        body(context),
+        date(context),
         photo(context),
+        count(context),
+        location(context),
       ],
     );
   }
 
-  Widget title(context) {
-    return Padding(
+  Widget date(context) {
+    return Container(
       padding: EdgeInsets.only(top: 4, left: 4, right: 4),
+      alignment: Alignment.center,
       child: Text(
         DateFormat.yMMMd().format(_post["date"].toDate()),
         style: Theme.of(context).textTheme.headline4,
@@ -29,22 +32,35 @@ class PostView extends StatelessWidget {
     );
   }
 
-  Widget body(context) {
+  Widget photo(context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.all(20),
+      child: Placeholder(),
+    );
+  }
+
+  Widget count(context) {
     return Container(
       margin: EdgeInsets.only(top: 10),
       padding: EdgeInsets.symmetric(horizontal: 8),
+      alignment: Alignment.center,
       child: Text(
-        _post['count'].toString(),
+        "${_post['count'].toString()} items",
         style: Theme.of(context).textTheme.headline6,
-        textAlign: TextAlign.left,
       ),
     );
   }
 
-  Widget photo(context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 16),
-      child: Placeholder(),
+  Widget location(context) {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      alignment: Alignment.center,
+      child: Text(
+        "location(lon, lat)",
+        style: Theme.of(context).textTheme.headline6,
+      ),
     );
   }
 }
